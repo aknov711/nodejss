a6fa8bcc2e6cce12fcf08fd9fafead7af5f39dd3
aknov711 <61695228+aknov711@users.noreply.github.com>
2020-03-28 18:14:33 +0530

    simple dfs approach

---
+++ dfs.cpp
@@ -0,0 +1,19 @@
+//the code for simple dfs
+vector<ll,ll>adj[n+1];
+bool visited[n+1];
+void dfs(int s)
+{
+  visited[s]=true;
+  for(auto it=adj[s].begin();it!=adj[s].end();it++)
+  {
+      if(!visited[*it])
+      dfs(*it);
+  }
+}
+for(ll i=0;i<=n;i++)
+{
+  if(!visited[i])
+  {
+       dfs(i);
+  }
+}
